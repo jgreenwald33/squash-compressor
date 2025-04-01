e6bcd5a6e6dc5a7d55567b997f777cfc273b1795
Jacob Greenwald <jakelgreenwald@gmail.com>
2025-04-01 16:14:21 -0500

    Added lines to compression plot

--- plugin/ui/squash-ui/src/App.tsx
+++ plugin/ui/squash-ui/src/App.tsx
@@ -1,27 +1,35 @@
 import './App.css'
+import { useState } from 'react'
 import { Card, Container, Group, Stack, Title } from '@mantine/core'
-import { AreaChart } from '@mantine/charts'
+import { LineChart } from '@mantine/charts'
 import SliderGroup from './components/SliderGroup'
 
-function App() {
 
+function App() {
+  const [threshold,setThreshold] = useState(-30);
+  const [knee,setKnee] = useState(10);
   return (
-    <Container w={"100vw"} h={"100vh"} p={"xl"} bg={""}>
-      <Stack h={"100%"} justify='space-between'>
+    <Container w={"100vw"} h={"100vh"} px={"xl"} py={"sm"} bg={""}>
+      <Stack h={"100%"} justify='space-between' gap={"sm"}>
         <Title w={"100%"} style={{textAlign:"start"}}>
           LOGO
         </Title>
         <Card shadow="sm" padding="lg" radius="md" withBorder h={"50%"} display={"flex"} style={{justifyContent:"center"}}>
-          <AreaChart 
-            h={"100%"} 
-            withTooltip={false}
-            referenceLines={[]}
-            dataKey='audio'
-            yAxisProps={{ domain: [-60, 0] }} 
-            xAxisProps={{domain: [-60,0]}}
-            data={[{x:0, y:0}]}
-            series={[{ name: 'audio', color: 'indigo.6' }]}
-          />
+        <LineChart 
+          h="100%"
+          withDots={false} 
+          withTooltip={false}
+          tickLine="none"
+          gridAxis="xy"
+          withXAxis
+          withYAxis
+          dataKey="xAxisProps"
+          yAxisProps={{ type:"number", domain: [-60, 0] }} 
+          xAxisProps={{ type: "number", dataKey: "x", domain: [-60, 0] }}
+          data={[{ x: -60, baseline: -60,adjusted:-60 },{ x: -30, baseline: threshold, adjusted:threshold }, { x: 0, baseline: 0, adjusted:0 - knee },]}
+          series={[{ name: 'baseline', color: 'indigo.6' }, {name:'adjusted', color:'red'}]}
+          curveType="linear"
+        />
         </Card>
 
         <Group justify='space-between'>
--- plugin/ui/squash-ui/src/components/SliderGroup.tsx
+++ plugin/ui/squash-ui/src/components/SliderGroup.tsx
@@ -1,4 +1,4 @@
-import { AngleSlider, Stack, Text, NumberInput } from "@mantine/core"
+import { AngleSlider, Stack, Text, NumberInput, Paper } from "@mantine/core"
 import { useState } from "react"
 
 interface Props {
@@ -12,29 +12,42 @@ interface Props {
 export default function SliderGroup({max,min,effectName,units}:Props) {
 
     const updateValue = (value:number) => {
-        let convertedValue:number = Math.round((value - 20) * ((max + 20) / 360))
+        let convertedValue:number = Math.round((value - 20) * ((max + 12) / 360))
         setValue(convertedValue)
     }
 
     const [value,setValue] = useState<string | number>(min);
-    const markRange = Array.from({ length: 361 }, (_, i) => i)
-                    .filter(i => i >= 20 && i <= 340)
-                    .map(i => ({ value: i, }));
     return (
         <Stack align="center">
             <Text>
                 {effectName}
             </Text>
-            <AngleSlider className={"flip"}
-                style={{transform:"rotate(180deg)"}}
-                marks={markRange}
-                  onChange={(e) => {
-                    updateValue(Number(e));
-                }}
-                  restrictToMarks
-                  withLabel={false}
-                  defaultValue={20}
-            />
+            <Paper
+                bg={"#e0e0e0"}
+                shadow="xl" 
+                style={{borderRadius:"9999px", justifyContent:"center", alignItems:"center"}} 
+                p={"10px"} 
+                display={"flex"} 
+            >
+                <Paper
+                    bg={"#fff"}
+                    style={{borderRadius:"9999px", justifyContent:"center", alignItems:"center", boxShadow:"0 8px 4px -1px gray"}} 
+                    p={"10px"} 
+                    display={"flex"}
+                    
+                >
+                    <AngleSlider className={"flip"}
+                        bg={"#fff"}
+                        onChange={(e) => {
+                            updateValue(Number(e));
+                        }}
+                        withLabel={false}
+                        defaultValue={20}
+                    />
+                </Paper>
+
+            </Paper>
+
             <NumberInput
                 w={"100px"} 
                 value={value}
