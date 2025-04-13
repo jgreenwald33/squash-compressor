ed2895c772747f7b893fd526f74fc5ab2f8426de
Jacob Greenwald <jakelgreenwald@gmail.com>
2025-04-13 12:46:44 -0500

    Added emitted events for communication with JUCE backend

--- plugin/ui/squash-ui/src/App.tsx
+++ plugin/ui/squash-ui/src/App.tsx
@@ -1,59 +1,69 @@
 import './App.css'
 import * as Juce from "./juce/index.js";
-import { useEffect, useState } from 'react'
-import { Card, Container, Group, Paper, Slider, Stack, Title } from '@mantine/core'
-import { LineChart } from '@mantine/charts'
-import SliderGroup from './components/SliderGroup'
+// import { useState } from 'react'
+import { Button, Container, Group, Paper, Stack, Title } from '@mantine/core'
+// import { LineChart } from '@mantine/charts'
+// import SliderGroup from './components/SliderGroup'
+import NumberSlider from './components/NumberSlider.js';
 
 function App() {
-  const [threshold] = useState(-30);
-  const [knee,setKnee] = useState(10);
-  useEffect(()=>{
-    console.log(window.__JUCE__.backend)
-  },
-  [])
+  // events for JUCE Backend
+  function emitRatioEvent(ratioVal:number) {
+    window.__JUCE__.backend.emitEvent("ratioUpdate", {
+        ratioVal: ratioVal
+      });
+  }
+
+  function emitGainEvent(gainVal:number) {
+    window.__JUCE__.backend.emitEvent("gainUpdate", {
+        gainVal: gainVal
+      });
+  }
+
+  function emitThresholdEvent(thresholdVal:number) {
+    window.__JUCE__.backend.emitEvent("thresholdUpdate", {
+      thresholdVal: thresholdVal
+    });
+  }
+
+  function emitAttackEvent(attackVal:number) {
+    window.__JUCE__.backend.emitEvent("attackUpdate", {
+      attackVal: attackVal
+    });
+  }
+
+  function emitReleaseEvent(releaseVal:number) {
+    window.__JUCE__.backend.emitEvent("releaseUpdate", {
+      releaseVal: releaseVal
+    });
+  }
+
+  function emitDryWetEvent(dryWetVal:number) {
+    window.__JUCE__.backend.emitEvent("dryWetUpdate", {
+      dryWetVal: dryWetVal
+    });
+  }
+
   return (
     <Container w={"100vw"} h={"100vh"} px={"xl"} py={"xl"} bg={""}>
       <Stack h={"100%"} justify='space-between' gap={"sm"}>
         <Title w={"100%"} style={{textAlign:"start"}}>
-          LOGO
+          Squash Compressor
         </Title>
-        <Card padding="lg" radius="md" withBorder h={"50%"} display={"flex"} style={{justifyContent:"center", borderColor:"#000"}}>
-        <LineChart 
-          h="100%"
-          withDots={false} 
-          withTooltip={false}
-          tickLine="none"
-          gridAxis="xy"
-          type="gradient"
-          gradientStops={[
-            { offset: 0, color: 'red.6' },
-            { offset: 20, color: 'orange.6' },
-            { offset: 40, color: 'yellow.5' },
-            { offset: 70, color: 'lime.5' },
-            { offset: 80, color: 'cyan.5' },
-            { offset: 100, color: 'blue.5' },
-          ]}
-          withXAxis
-          withYAxis
-          dataKey="xAxisProps"
-          yAxisProps={{ type:"number", domain: [-60, 0] }} 
-          xAxisProps={{ type: "number", dataKey: "x", domain: [-60, 0] }}
-          data={[{ x: -60, baseline: -60,adjusted:-60 },{ x: -30, baseline: threshold }, { x: 0, baseline: 0 - knee, }]}
-          series={[{ name: 'baseline', color: 'gray.6' }]}
-          curveType="linear"
-        />
-        </Card>
-        <Slider onChange={setKnee} value={knee} min={0} step={1} max={100}/>
-        
-        <Paper style={{border:"1px solid #000"}} p={"20px"} radius={"md"}>
+        <Paper p={"20px"} radius={"md"} px={0}>
           <Group justify='space-between'>
-            <SliderGroup min={0} max={20} effectName='Ratio'/>
+            {/* <SliderGroup min={0} max={20} effectName='Ratio'/>
             <SliderGroup min={-60} max={0} step={0.5} effectName='Gain' units='db'/>
             <SliderGroup min={0} max={100} effectName='Threshold' units='db'/>
             <SliderGroup min={0} max={100} effectName='Attack' units='ms' />
             <SliderGroup min={0} max={100} effectName='Release' units='ms'/>
-            <SliderGroup min={0} max={100} effectName='Dry/Wet' units='%'/>
+            <SliderGroup min={0} max={100} effectName='Dry/Wet' units='%'/> */}
+            <NumberSlider min={0} max={20} effectName='Ratio' onChangeEvent={emitRatioEvent}/>
+            <NumberSlider min={-60} max={0} step={0.5} effectName='Gain' units='db' onChangeEvent={emitGainEvent}/>
+            <NumberSlider min={0} max={100} effectName='Threshold' units='db' onChangeEvent={emitThresholdEvent}/>
+            <NumberSlider min={0} max={100} effectName='Attack' units='ms' onChangeEvent={emitAttackEvent} />
+            <NumberSlider min={0} max={100} effectName='Release' units='ms' onChangeEvent={emitReleaseEvent}/>
+            <NumberSlider min={0} max={100} effectName='Dry/Wet' units='%' onChangeEvent={emitDryWetEvent}/>
           </Group>
         </Paper>
 
